cccacfc7aad78781831efc3bc3a389e5401492a6
k8blanco <katiewhite303@gmail.com>
2019-03-28 16:27:23 -0600

    adding Portfolio to base github io papage

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # k8blanco.github.io
+
+- Welcome to my portfolio!  I'm a Fullstack Developer based out of Denver, CO.  I love
+making sleek, cool UIs which usually utilize Google Material Design concepts and research, and I'm (only slightly) obsessed with color theory.
+- Databases and data are my jam, and while I work with both mySQL and mongoDB, I tend to prefer mongoDB.  Node.js and I are best buds, and it's a rare instance that I don't immediately npm i colors (see? there are those colors again).
+
+- In my free time I enjoy painting, reading, coding, and llamas.  My greatest dream is
+to find a project that incorporates all my loves, but alas, I have yet to find a way 
+to include llamas in my code.  One day, though.  One day.
---
+++ assets/javascript/portfolio2.js
@@ -0,0 +1,77 @@
+//Portfolio 2
+var demoLink = "";
+
+
+$(document).ready(function(){
+
+    //Materialize Initializers and Options/Functions
+    $('.sidenav').sidenav({
+      isFixed: true
+    });
+
+    $('.fixed-action-btn').floatingActionButton({
+      direction: "left"
+    });
+
+    $(".dropdown-trigger").dropdown({
+      closeOnClick: true
+    });
+
+    $(".carousel").carousel();
+
+    $('.modal').modal();
+    
+    //add listeners for screen size
+    // carouselSize(x);
+    // x.addListener(carouselSize);
+
+    //media query function for carousel size
+    // carouselSize();
+
+    //carousel oncycle to function
+    $(".carousel.carousel-slider").carousel({
+      fullWidth: true,
+      indicators: true,
+      onCycleTo: function(ele) {
+        //console log entire element and index of element
+        console.log(ele);
+        console.log($(ele).index());
+        //assign demoLink
+        demoLink = $("#" + ele.id).attr("href");
+        console.log("element id: ", ele.id);
+        $(".demoBtn").attr("href", demoLink);
+        console.log("demo link: ", demoLink);
+
+      }
+    });
+    $(".demoBtn").on("click", function() {
+      console.log("you clicked me! My href is: ", demoLink);
+    })
+ 
+  });
+  
+
+// function carouselSize(x) {
+//   if (x.matches) {
+//     $(".carousel.carousel-slider").carousel({
+//       fullWidth: true,
+//       indicators: true,
+//       numVisible: 1
+      
+//     });
+//   } else {
+//     $(".carousel.carousel-slider").carousel({
+//       fullWidth: false,  
+//       indicators: false,
+//       numVisible: 3
+//     });
+//   }
+// }
+
+// var x = window.matchMedia("(max-width: 750px)");
+
+
+  
+  // $("$demo").on("click", function(){
+
+  // };
