b6d723c2ba253fe4998d802c8c28d094483c8d9f
k8blanco <katiewhite303@gmail.com>
2019-05-19 19:02:23 -0600

    added typewriter effect to job tiltle and copyright, plus llama easter egg

--- assets/javascript/portfolio2.js
+++ assets/javascript/portfolio2.js
@@ -1,69 +1,46 @@
-//Portfolio 2
-var demoLink = "";
+//Portfolio
+
+$(document).ready(function () {
+
+  //Materialize Initializers and Options/Functions
+  // $('.sidenav').sidenav({
+  //   isFixed: true
+  // });
+
+  $('.parallax').parallax();
+
+  $('.fixed-action-btn').floatingActionButton({
+    direction: "left"
+  });
+
+  // $(".dropdown-trigger").dropdown({
+  //   closeOnClick: true
+  // });
+
+  //typing.js - no loop?
+  //set selector, speed, and initial delay
+  // _$("demo", 100, 800)
+  //   //set options
+  //   .type("FULL ", {
+  //     fontWeight: "400",
+  //     color: "white",
+  //   }).wait(1000).speed(100)
+  //   .lineBreak()
+  //   .type("STACK ", {
+  //     fontWeight: "400",
+  //     color: "white",
+  //   }).wait(300).speed(100)
+  //   .lineBreak()
+  //   .type("DEVELOPRE ", {
+  //     fontWeight: "400",
+  //     color: "white",
+  //   }).wait(300).speed(100)
+  //   .delete("RE ").wait(300).speed(100)
+  //   .type("ER ", {
+  //     fontWeight: "400",
+  //     color: "white",
+  //   }).wait(300).speed(100);
+
+});
 
 
-$(document).ready(function(){
-
-    //Materialize Initializers and Options/Functions
-    $('.sidenav').sidenav({
-      isFixed: true
-    });
-
-    $('.parallax').parallax(); 
-
-    $('.fixed-action-btn').floatingActionButton({
-      direction: "left"
-    });
-
-    $(".dropdown-trigger").dropdown({
-      closeOnClick: true
-    });
-
-    $(".carousel").carousel();
-
-    $('.modal').modal();
-    
-    //add listeners for screen size
-    // carouselSize(x);
-    // x.addListener(carouselSize);
-
-    //media query function for carousel size
-    // carouselSize();
-
-    //carousel oncycle to function
-    $(".carousel.carousel-slider").carousel({
-      fullWidth: true,
-      indicators: true,
-      onCycleTo: function(ele) {
-        //console log entire element and index of element
-        console.log(ele);
-        console.log($(ele).index());
-        //assign demoLink
-        demoLink = $("#" + ele.id).attr("href");
-        console.log("element id: ", ele.id);
-        $(".demoBtn").attr("href", demoLink);
-        console.log("demo link: ", demoLink);
-
-      }
-    });
-    $(".demoBtn").on("click", function() {
-      console.log("you clicked me! My href is: ", demoLink);
-    })
- });
-
-//!! LOAD INTO NPM !! typewriter.js
-// var app = document.getElementById('app');
-
-// var typewriter = new Typewriter(app, {
-//     loop: true
-// });
-
-// typewriter.typeString('Full Stack Developer')
-//     .pauseFor(2500)
-//     .deleteAll()
-//     .typeString('Strings can be removed')
-//     .pauseFor(2500)
-//     .deleteChars(7)
-//     .typeString('<strong>altered!</strong>')
-//     .pauseFor(2500)
-//     .start();
